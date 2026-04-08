3da021a47fa3d675887ebc29bb1e4342570abfda
aonontojahan <aonontojahan@gmail.com>
2026-04-09 02:34:24 +0600

    chore: remove obsolete database and API test scripts

--- test_db.py
+++
@@ -1,6 +0,0 @@
-
-from backend.models import ListingStatus
-from sqlalchemy import Enum
-cols = Enum(ListingStatus, values_callable=lambda obj: [e.value for e in obj])
-print('Enum mapped values:', cols.enums)
-
